7725c43550245d926330ace105a2635eee3ade10
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-06-05 12:14:24 +0200

    minor improvements..

--- GeoTag-MobileApp/app/src/main/java/geotag/it/geotag_mobileapp/MainActivity.java
+++ GeoTag-MobileApp/app/src/main/java/geotag/it/geotag_mobileapp/MainActivity.java
@@ -2,12 +2,26 @@ package geotag.it.geotag_mobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText tag_txt;
+    private RecyclerView rv;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tag_txt = findViewById(R.id.tag_txt);
+        rv = findViewById(R.id.rv);
+    }
+
+    public void tag(View v){
+        tag_txt.setText("Btn Clicked");
     }
 }
